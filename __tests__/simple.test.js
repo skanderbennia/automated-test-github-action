4247780badf80f22b35aeb3a4e3fd3f489d5ea47
Mohamed Skander Bennia <skander.bennia@avaxia-group.com>
2023-03-23 11:32:23 +0100

    second commit

--- __tests__/simple.test.js
+++ __tests__/simple.test.js
@@ -1,3 +1,12 @@
-test("1 + 1 = 2", () => {
-  expect(1 + 1).toBe(2);
+const data = [
+  { id: "1", name: "Product 1", price: 100 },
+  { id: "2", name: "Product 2", price: 200 },
+  { id: "3", name: "Product 3", price: 300 }
+];
+test("Expect to return data from the moched database", () => {
+  const expectedAttributes = ["id", "name", "price"];
+
+  data.forEach((item) => {
+    expect(Object.keys(item)).toEqual(expect.arrayContaining(expectedAttributes));
+  });
 });
